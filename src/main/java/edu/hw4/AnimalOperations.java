05ae844ac6648dd8f905ceb40de7992a5834c2da
Kirill <kirill.zheludev@yandex.ru>
2023-11-01 23:51:10 +0300

    fix codestyle

--- src/main/java/edu/hw4/AnimalOperations.java
+++ src/main/java/edu/hw4/AnimalOperations.java
@@ -1,6 +1,7 @@
 package edu.hw4;
 
 import java.util.Comparator;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -9,7 +10,6 @@ import java.util.Set;
 import java.util.function.BinaryOperator;
 import java.util.function.Function;
 import java.util.stream.Collectors;
-import java.util.Collection;
 
 public class AnimalOperations {
     private AnimalOperations() {
@@ -58,10 +58,10 @@ public class AnimalOperations {
     }
 
     public static Map<Animal.Type, Animal> getMostHeavyAnimalsEachType(List<Animal> animals) {
-        return animals.
-            stream()
-            .collect(Collectors.
-                toMap(
+        return animals
+            .stream()
+            .collect(Collectors
+                .toMap(
                     Animal::type,
                     Function.identity(),
                     BinaryOperator.maxBy(Comparator.comparing(Animal::weight))
